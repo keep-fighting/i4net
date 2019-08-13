8555e0303e10798947dcb6bb68e2d641f555522f
Snow <Snow.zheng@makeding.com>
2019-08-14 01:45:59 +0800

    创建文章管理模块

--- article/urls.py
+++ article/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'article'
 urlpatterns = [
+    # 首页，列表页
+    path('', views.index, name='index'),
 ]
--- article/views.py
+++ article/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    pass
+    return render(request, 'article/index.html', locals())
