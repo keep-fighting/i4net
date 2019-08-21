26daf4a4f2d2e3e93edcbf4975aebdf6a2abb98c
Snow <Snow.zheng@makeding.com>
2019-08-21 15:26:33 +0800

    文章详情页 - MarkDown语法高亮

--- article/views.py
+++ article/views.py
@@ -1,3 +1,4 @@
+import markdown
 from django.views import generic
 from .models import ArticlePost
 
@@ -14,3 +15,15 @@ class ArticleDetailView(generic.DetailView):
     template_name = 'article/detail.html'
     model = ArticlePost
     context_object_name = 'article'
+
+    def get_object(self, queryset=None):
+        article = super().get_object()
+        article.body = markdown.markdown(
+            article.body,
+            extensions=[
+                # 包含 缩写、表格等常用扩展
+                'markdown.extensions.extra',
+                # 语法高亮扩展
+                'markdown.extensions.codehilite',
+            ])
+        return article
